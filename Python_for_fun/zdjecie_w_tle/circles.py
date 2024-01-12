d394a44ca59d82431abefd2cd68d70cd39802574
Pablo1644 <62251949+Pablo1644@users.noreply.github.com>
2024-01-12 08:35:09 +0100

    Update circles.py

--- Python_for_fun/zdjecie_w_tle/circles.py
+++ Python_for_fun/zdjecie_w_tle/circles.py
@@ -19,14 +19,4 @@ for i in range(6,126,6):
     draw_grad.rectangle(canv_temp,fill=(colors[0]+int(i/2),colors[1]+int(i/2),colors[2])) # changed from 181,172, 0
 img.show()
 img.save('bg.jpg')
-# drawing Circles
-# r = 40
-# for j in range(12):
-#     r-=3
-#     left_up_point = (center - r,center - r)
-#     right_down_point=(center+r,center+r)
-#     elipse_shape = (left_up_point,right_down_point)
-#     draw_grad.ellipse(elipse_shape,fill=(200+int(4*j),50+j,10+j))
-
-# img.show()
 
